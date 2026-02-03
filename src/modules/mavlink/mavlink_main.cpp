7ae95c47ba02db2b76c44336864602727985679f
Yulian Oifa <yulian.oifa@mobius-software.com>
2026-02-03 13:45:14 +0200

    fixing typo

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1431,7 +1431,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("EFI_STATUS", 0.1f);
 		configure_stream_local("ESC_INFO", 0.1f);
 		configure_stream_local("ESC_STATUS", 10.0f);
-		configure_stream_local("SERVO_OUTPUT_RAW_0", 10f);
+		configure_stream_local("SERVO_OUTPUT_RAW_0", 10.0f);
 		configure_stream_local("HIGHRES_IMU", 10.0f);
 		configure_stream_local("ESTIMATOR_STATUS", 0.1f);
 		configure_stream_local("EXTENDED_SYS_STATE", 1.0f);
